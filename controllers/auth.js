957a6bf326a51e2336f27939409d0870fafafa5c
Reanz_Arthur <reqbahrf@gmail.com>
2025-08-29 13:23:34 +0800

    feat: Implement user authentication with JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
-const login = (req, res) => {
-  res.send('Login router');
+import User from '../models/User.js';
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
+import { StatusCodes } from 'http-status-codes';
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw new BadRequestError('Please provide the email and password');
+  const user = await User.findOne({ email });
+  if (!user) throw new UnauthenticatedError('Invalid credentials');
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) throw new UnauthenticatedError('Invalide credentials');
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ username: user.name, token });
 };
-const register = (req, res) => {
-  res.send('Register router');
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ username: user.name, token });
 };
 
 export { login, register };
---
+++ models/User.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minLength: [3, 'Name must be at least 3 characters'],
+    maxLength: [20, 'Name must be less than 20 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minLength: [8, 'Password must be at least 8 characters'],
+  },
+});
+
+UserSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, username: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = async function (userProvidedPassword) {
+  const isMatch = await bcrypt.compare(userProvidedPassword, this.password);
+  return isMatch;
+};
+export default mongoose.model('User', UserSchema);
