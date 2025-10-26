b07283ebb06f39b2b9f1f3ca121aef8736a4e944
Reanz_Arthur <reqbahrf@gmail.com>
2025-10-26 18:02:29 +0800

    feat(company): Refactor CompanyCard to use modal for details

--- frontend/src/components/company/CompanyCard.tsx
+++ frontend/src/components/company/CompanyCard.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { CompanyInfo } from '../../types/company';
+import CompanyModalBody from './CompanyModalBody';
 
-import {
-  RiMapPin2Line,
-  RiGlobalLine,
-  RiMailLine,
-  RiPhoneLine,
-} from '@remixicon/react';
+import { useModalContext } from '@/context/ModalContext';
 
 interface CompanyCardProps {
   company: CompanyInfo;
 }
 
 const CompanyCard: React.FC<CompanyCardProps> = ({ company }) => {
+  const { openModal } = useModalContext();
+
   return (
-    <div className='w-full rounded-lg bg-white p-6 shadow-md transition-shadow duration-300 hover:shadow-lg dark:bg-gray-800'>
+    <div
+      className='group w-full overflow-hidden rounded-lg bg-white p-6 shadow-md transition-shadow duration-300 hover:cursor-pointer hover:bg-gray-100 hover:shadow-lg dark:bg-gray-800 dark:hover:bg-gray-600 dark:hover:shadow-lg'
+      onClick={() =>
+        openModal(
+          <CompanyModalBody company={company} />,
+          'md',
+          company.companyName,
+        )
+      }
+    >
       <div className='flex flex-col items-start justify-between sm:flex-row sm:items-center'>
         <div className='flex items-center'>
           <img
@@ -32,55 +39,19 @@ const CompanyCard: React.FC<CompanyCardProps> = ({ company }) => {
             <p className='text-sm text-gray-500 dark:text-gray-400'>
               Industry: {company.industry}
             </p>
-            <div className='flex flex-row gap-1'>
-              <div className='flex items-center text-sm'>
-                <RiMapPin2Line
-                  className='mr-3 flex-shrink-0 text-gray-500 dark:text-gray-400'
-                  size={20}
-                />
-                <span>{company.address}</span>
-              </div>
-
-              <div className='flex items-center'>
-                <RiMailLine
-                  className='mr-3 flex-shrink-0 text-gray-500 dark:text-gray-400'
-                  size={20}
-                />
-                <span className='truncate'>{company.contactEmail}</span>
-              </div>
-              <div className='flex items-center text-sm'>
-                <RiPhoneLine
-                  className='mr-3 flex-shrink-0 text-gray-500 dark:text-gray-400'
-                  size={20}
-                />
-                <span>{company.contactPhone}</span>
-              </div>
-              <div className='flex items-center text-sm'>
-                <RiGlobalLine
-                  className='mr-3 flex-shrink-0 text-gray-500 dark:text-gray-400'
-                  size={20}
-                />
-                <a
-                  href={company.website}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='truncate text-blue-600 hover:underline dark:text-blue-400'
-                >
-                  {company.website}
-                </a>
-              </div>
-            </div>
           </div>
         </div>
-        <span
-          className={`mt-4 self-start rounded-full px-4 py-1 text-sm font-bold tracking-wide uppercase sm:mt-0 ${
-            company.status === 'approved'
-              ? 'bg-green-600 text-white'
-              : 'bg-gray-600 text-white'
-          }`}
-        >
-          {company.status}
-        </span>
+        <div className='flex flex-col self-start'>
+          <span
+            className={`mt-4 self-start rounded-full px-4 py-1 text-sm font-bold tracking-wide uppercase sm:mt-0 ${
+              company.status === 'approved'
+                ? 'bg-green-600 text-white'
+                : 'bg-gray-600 text-white'
+            } group-hover:bg-gray-700`}
+          >
+            {company.status}
+          </span>
+        </div>
       </div>
       <hr className='my-4 border-t border-gray-200 dark:border-gray-700' />
     </div>
